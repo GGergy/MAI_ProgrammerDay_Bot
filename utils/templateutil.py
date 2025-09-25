9538484a4f66b4809c9949888cb16f2195ceac60
andrey-delovoy <self.education.1411@gmail.com>
2025-09-25 11:38:14 +0300

    Improments of SnakeMap class. Hide all inner class methods. Rewrited gap logic

--- utils/templateutil.py
+++ utils/templateutil.py
@@ -28,29 +28,52 @@ class SnakeMap():
         self.horizontal_separator = "-"
         self.vertical_separator = "|"
         self.vertical_path_height = 3
-        
-    def generate_vertical_path(self, width:int) -> str:
+    
+    def __get_portions(self, length:int, value:int, reversed:bool=False) -> list:
+        '''
+        Examples:
+        (4, 16) -> [4, 4, 4, 4]
+        (5, 16) -> [4, 3, 3, 3, 3]
+        (5, 16, True) -> [3, 3, 3, 3, 4]
+        '''
+        # Special cases
+        if length == 0:
+            return []
+        # Algorithm
+        result = [0 for _ in range(length)]
+        addition = value // length
+        for i in range(length):
+            if i < value % length:
+                result[i] += 1
+            result[i] += addition
+        if reversed:
+            result.reverse()
+        return result
+    
+    def __generate_vertical_path(self, width:int) -> str:
         vertical_path = ""
         for i in range(self.vertical_path_height):
             vertical_path += "\n" + " "*(width-len(self.vertical_separator)) + self.vertical_separator
         return vertical_path
     
-    def generate_row(self, chunk:list[str]) -> str:
+    def __generate_row(self, chunk:list[str]) -> str:
         elems_length = sum(len(elem) for elem in chunk)
         total_gap_length = self.width_of_text-elems_length
         pattern = (" " + self.horizontal_separator)*self.width_of_text
-        one_gap_length = total_gap_length // (len(chunk)-1)
-        last_gap_length = total_gap_length % (len(chunk)-1)
+        gaps_length_list = self.__get_portions(len(chunk)-1, total_gap_length)
         
         row = chunk[0]
-        for i, chunk_elem in enumerate(chunk[1:-1]):
-            
-            gapper = pattern[i*one_gap_length:(i+1)*one_gap_length]
-            row += gapper + chunk_elem
-        row += pattern[0:(one_gap_length + last_gap_length)] + chunk[-1]
+        pos = 0
+        for i, chunk_text in enumerate(chunk[1:]):
+            gap_text = pattern[pos:(pos+gaps_length_list[i])]
+            pos += gaps_length_list[i]
+            row += gap_text + chunk_text
         return row
     
     def generate_snake_map(self, list_of_elems:list[str], width:int) -> str:
+        '''Generates snake like map using given list of elements'''
+        #TODO if len(list_of_elems) % width == 1: UEB
+        
         # Algorithm
         chunks = [list_of_elems[i:i + width] for i in range(0, len(list_of_elems), width)]
         
@@ -59,14 +82,14 @@ class SnakeMap():
             if i % 2 == 1:
                 chunks[i].reverse()
         #rows = [horizontal_path.join(chunk) for chunk in chunks]
-        rows = [self.generate_row(chunk) for chunk in chunks]
+        rows = [self.__generate_row(chunk) for chunk in chunks]
         
         # Gathering result map
         result = "\n" + rows[0]
         for i, row in enumerate(rows[1:]):
             if i % 2 == 0:
-                result += self.generate_vertical_path( len(row) )
+                result += self.__generate_vertical_path( len(row) )
             else:
-                result += self.generate_vertical_path( 0 )
+                result += self.__generate_vertical_path( 0 )
             result += "\n" + row
         return result
